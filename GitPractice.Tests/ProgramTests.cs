f78af0b223ab6c0d870ea9476c7b5703076657e4
Artem Volk <artem_volk04@mail.ru>
2026-03-26 19:53:54 +0500

    Reapply "ПОПРАВИЛИ ВЫВОД В ТЕСТЕ"
    This reverts commit 02bd88be51e4b3bdb6c8b88eb24683f8de7939f9.

--- GitPractice.Tests/ProgramTests.cs
+++ GitPractice.Tests/ProgramTests.cs
@@ -9,6 +9,6 @@ public class ProgramTests
         var result = Program.Greet();
 
         // Assert
-        Assert.Equal("Привет, Git!", result);
+	Assert.Equal("Добро пожаловать в GitPractice!", result);
     }
 }
