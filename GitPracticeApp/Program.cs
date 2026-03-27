ac47eda6399e1e88ac57e367db16f4ee468036ca
Alexandr Iarmoshenko <a2398253@gmail.com>
2026-03-27 12:32:08 +0500

    Заменил метод приветствия Greet на FavoriteColor

--- GitPracticeApp/Program.cs
+++ GitPracticeApp/Program.cs
@@ -4,11 +4,11 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine(Greet());
+        Console.WriteLine(FavoriteColor());
     }
 
-    public static string Greet()
+    static string FavoriteColor()
     {
-        return "Привет, Git от преподавателя!";
-    }
+        return "Любимый цвет";
+    } 
 }
