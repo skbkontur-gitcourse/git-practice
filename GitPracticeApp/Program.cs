102efbfbc840ba23560ecf2294285cc00b5ca1ae
Georgy Kabitsky <georgijkabickij1@gmail.com>
2026-04-01 11:20:34 +0500

    Update class Program. Add FC

--- GitPracticeApp/Program.cs
+++ GitPracticeApp/Program.cs
@@ -7,8 +7,8 @@ public class Program
         Console.WriteLine(Greet());
     }
 
-    public static string Greet()
-    {
-        return "Привет, Git от преподавателя!";
-    }
+      static string FavoriteColor()
+{
+    return "Любимый цвет";
+}
 }
